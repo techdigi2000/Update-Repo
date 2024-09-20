251622d9f4e31e304690b95ae3b19e84db5ecc3e
techdigi2000 <techdigi307@gmail.com>
2024-09-20 16:53:45 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,222 +1,87 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet,
+  createRoutesFromElements,
+  Route,
+  ScrollRestoration,
+} from "react-router-dom";
+import Footer from "./components/home/Footer/Footer";
+import FooterBottom from "./components/home/Footer/FooterBottom";
+import Header from "./components/home/Header/Header";
+import HeaderBottom from "./components/home/Header/HeaderBottom";
+import SpecialCase from "./components/SpecialCase/SpecialCase";
+import SignIn from "./pages/Account/SignIn";
+import SignUp from "./pages/Account/SignUp";
+import Cart from "./pages/Cart/Cart";
+import Contact from "./pages/Contact/Contact";
+import Home from "./pages/Home/Home";
+import Journal from "./pages/Journal/Journal";
+import Offer from "./pages/Offer/Offer";
+import Payment from "./pages/payment/Payment";
+import ProductDetails from "./pages/ProductDetails/ProductDetails";
+import Shop from "./pages/Shop/Shop";
+import Setup from "./pages/Setup/Setup";
+import Downloading from "./pages/Downloading/Downloading";
 
-function Setup() {
-  React.useEffect(() => {
-    // Preload important images
-    const imagesToPreload = [
-      "https://123.hp.com/resources/assets/img/hp-logo.svg",
-      "https://123.hp.com/resources/assets/img/defaultPrinterSetup.png",
-      "https://123.hp.com/resources/assets/img/person_assist.svg",
-    ];
-    imagesToPreload.forEach((src) => {
-      const img = new Image();
-      img.src = src;
-    });
-  }, []);
+import Loading from "./pages/Loading/Loading";
+import Error from "./pages/Error/Error";
+import Privacypolicy from "./pages/privacy/Privacypolicy";
+import Terms from "./pages/Terms/Terms";
+import AboutUs from "./pages/AboutUs/AboutUs";
+import BannerBottom from "./components/Banner/BannerBottom";
 
+const Layout = () => {
   return (
-    <>
-      {/* Keep all your existing CSS links */}
-      <link
-        media="screen"
-        rel="stylesheet"
-        href="https://123.hp.com/resources/assets/css/page/123fontstyles.css"
-      />
-      <link
-        media="screen"
-        rel="stylesheet"
-        href="https://123.hp.com/resources/assets/dist/application.min.css"
-      />
-      <link
-        rel="stylesheet"
-        href="https://123.hp.com/resources/assets/css/vendor/jquery.typeahead.min.css"
-        type="text/css"
-      />
-      <link
-        rel="stylesheet"
-        href="https://123.hp.com/resources/assets/css/page/flex-design.css"
-        type="text/css"
-      />
-      <link
-        rel="stylesheet"
-        href="https://123.hp.com/resources/assets/css/page/index.css"
-        type="text/css"
-      />
-      <link
-        rel="stylesheet"
-        href="https://123.hp.com/resources/assets/css/vendor/owlCarousel/owl.carousel.css"
-        type="text/css"
-      />
-      <link
-        rel="stylesheet"
-        href="https://123.hp.com/resources/assets/css/vendor/owlCarousel/owl.theme.default.css"
-        type="text/css"
-      />
-      <link
-        rel="stylesheet"
-        href="https://www8.hp.com/etc.clientlibs/HPIT-AEM-GLOBALNAV/clientlibs-globalnav/clientlibs-fonts/us/en/clientlib-hf-fontface.e416b85cad0cf264bdbe92bc18c4af0a.css"
-        type="text/css"
-        media="screen, print"
-      />
-      <link
-        rel="stylesheet"
-        href="https://www8.hp.com/etc.clientlibs/HPIT-AEM-GLOBALNAV/clientlibs-globalnav/clientlib-hpi-hf-r-css.e416b85cad0cf264bdbe92bc18c4af0a.css"
-        type="text/css"
-        media="screen, print"
-      />
+    <div>
+      <SpecialCase />
+      <ScrollRestoration />
+      <Outlet />
+    </div>
+  );
+};
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+      <Route path="/" element={<Layout />}>
+        {/* ==================== Header Navlink Start here =================== */}
+        <Route index element={<Home />}></Route>
+        <Route path="/shop" element={<Shop />}></Route>
 
-      <title>123.hp.com - Printer setup from the HP® Official site</title>
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <meta name="hp_inav_version" content="ihfcaas.5.r" />
+        <Route path="/contact" element={<Contact />}></Route>
+        <Route path="/journal" element={<Journal />}></Route>
+        <Route path="/privacy" element={<Privacypolicy />}></Route>
+        <Route path="/terms" element={<Terms />}></Route>
+        <Route path="/about-us" element={<AboutUs />}></Route>
+        {/* ==================== Header Navlink End here ===================== */}
+        <Route path="/offer" element={<Offer />}></Route>
+        <Route path="/product/:_id" element={<ProductDetails />}></Route>
+        <Route path="/cart" element={<Cart />}></Route>
+        <Route path="/paymentgateway" element={<Payment />}></Route>
+      </Route>
+      <Route path="/signup" element={<SignUp />}></Route>
+      <Route path="/signin" element={<SignIn />}></Route>
+      <Route path="/hp-setup-guide" element={<Setup />}></Route>
+      <Route path="/setup/downloading" element={<Downloading />}></Route>
 
-      {/* Rest of your component JSX */}
-      <div className="header-123" id="header-123">
-        <div className="header-container">
-          <div className="hp-logo">
-            <Link to="/">
-              <img
-                src="https://123.hp.com/resources/assets/img/hp-logo.svg"
-                width="50"
-                height="50"
-              />
-            </Link>
-          </div>
-          <ul className="header-menu">
-            <li>
-              <Link to="/setup/officejet">OfficeJet</Link>
-            </li>
-            <li>
-              <Link to="/setup/deskjet">DeskJet</Link>
-            </li>
-            <li>
-              <Link to="/setup/envy">ENVY</Link>
-            </li>
-            <li>
-              <Link to="/setup/laserjet">LaserJet</Link>
-            </li>
-          </ul>
-        </div>
-      </div>
+      <Route path="/setup/loading" element={<Loading />}></Route>
 
-      {/* Hidden inputs */}
-      <div>
-        <input id="deviceId" type="hidden" defaultValue="" />
-        <input id="languageDirection" defaultValue="ltr" type="hidden" />
-        <input
-          id="gotoHPUrl"
-          defaultValue="https://h20180.www2.hp.com/apps/Nav?h_product=known&h_client=S-A-10020-1&h_lang=en&h_cc=us&h_pagetype=s-002"
-          type="hidden"
-        />
-        <input id="localePath" defaultValue="/us/en" type="hidden" />
-        <input
-          id="printerSetupUrl"
-          defaultValue="https://support.hp.com/us-en/printer-setup"
-          type="hidden"
-        />
-      </div>
+      <Route path="/setup/error" element={<Error />}></Route>
 
-      <div id="one23-overlay" />
+      <Route path="/setup/officejet" element={<Downloading />}></Route>
+      <Route path="/setup/deskjet" element={<Downloading />}></Route>
+      <Route path="/setup/laserjet" element={<Downloading />}></Route>
+      <Route path="/setup/envy" element={<Downloading />}></Route>
+    </Route>
+  )
+);
 
-      <main className="vertical">
-        {/* Main content */}
-        <div className="standard-bg horizontal centered top-content shrink-off">
-          <div className="main-content vertical has-side-content">
-            <div className="page-header vertical centered shrink-off">
-              <div>Set up your HP printer</div>
-            </div>
-            <div className="sub-header">
-              Enter your HP product name and model number to get the right HP
-              Smart software and drivers for you
-            </div>
-            <div id="ljAlertMessage">&nbsp;</div>
-            <div id="printerlist-content" className="horizontal">
-              <div className="searchbar input">
-                <form action="" id="typeahead-form" name="typeahead-form">
-                  <div className="typeahead__container">
-                    <div className="typeahead__field">
-                      <span className="typeahead__query">
-                        <span className="input-cancel" />
-                        <input
-                          autoComplete="off"
-                          className="js-typeahead"
-                          id="printer-searchlist"
-                          name="printer-searchlist"
-                          required="required"
-                          spellCheck="false"
-                          title="Please fill out this field."
-                          type="search"
-                          placeholder="Enter your product name here. Example: OfficeJet 9010"
-                        />
-                      </span>
-                    </div>
-                  </div>
-                </form>
-              </div>
-              <div className="searchbar button">
-                <Link to="/setup/downloading" id="btn-begin-link">
-                  <button className="btn" disabled="" id="btn-begin">
-                    Next
-                  </button>
-                </Link>
-              </div>
-            </div>
-            <div className="whereis-printer-model">
-              <div className="no-device" id="device-not-listed">
-                <Link to="/setup/downloading">My device is not listed</Link>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        {/* Bottom content */}
-        <div className="horizontal centered">
-          <div className="main-content vertical bottom">
-            <div className="vertical">
-              <div className="side-images always-visible printer-container">
-                <img
-                  alt="printer"
-                  className="welcome-selected-printer"
-                  id="printers-image"
-                  src="https://123.hp.com/resources/assets/img/defaultPrinterSetup.png"
-                  width="300"
-                  height="200"
-                />
-              </div>
-              <div className="vertical main-text bottom-text">
-                <div id="copy1">
-                  Install HP Smart software and drivers on each mobile device or
-                  computer you want to print from. Add the printer on the new
-                  device.
-                </div>
-                <div className="support-item">
-                  <div className="support-header horizontal">
-                    <div className="support-header-icon">
-                      <img
-                        src="https://123.hp.com/resources/assets/img/person_assist.svg"
-                        alt="Support icon"
-                        width="24"
-                        height="24"
-                      />
-                    </div>
-                    <div className="support-header-note main-text" id="copy2">
-                      Need additional help with setup? Visit{" "}
-                      <a
-                        href="https://tawk.to/chat/66a8eee332dca6db2cb7758a/1i41uaahk"
-                        id="support-url"
-                      >
-                        HP Support
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </main>
-    </>
+function App() {
+  return (
+    <div className="font-bodyFont">
+      <RouterProvider router={router} />
+    </div>
   );
 }
 
-export default Setup;
+export default App;
